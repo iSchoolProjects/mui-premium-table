042b7ca0b92592bbb1d41edc2f659b872b99b595
nikolap13 <slobodan.bn@gmail.com>
2024-11-06 18:46:42 +0100

    fix headre implementation

--- script/components/header.js
+++ script/components/header.js
@@ -1,5 +1,25 @@
 import * as styles from '../../styles/header.module.css';
+import createNode from '../utils/createNode';
 
-export default function Header() {
-  return `<h1 class="${styles.header}">Header</h1>`;
+export default function Header(headerParent, keys, current, setCurrent) {
+  const container = createNode(headerParent, {
+    tag: 'div',
+    classes: [styles.header],
+  });
+
+  for (const key of keys) {
+    console.log(current);
+    createNode(container, {
+      tag: 'div',
+      classes: [styles.item],
+      text: key,
+      dataset: current.key === key ? current : null,
+    }).addEventListener('click', function () {
+      setCurrent((prev) => (prev.key === key ? {...prev, sort: (prev.sort + 1) % 3} : {key, sort: 1}));
+    });
+  }
 }
+
+// prvi klik sort na gore
+// drugi klik sort na dole
+// treci klik ugasi sort
