b1d6a16966ca38b7e52352bb10c1fda19ee3e737
nikolap13 <slobodan.bn@gmail.com>
2024-11-08 20:01:08 +0100

    feat: add dropdown options

--- script/components/header.js
+++ script/components/header.js
@@ -1,6 +1,23 @@
 import * as styles from '../../styles/header.module.css';
 import createNode from '../utils/createNode';
 
+// {}
+/**
+ * 
+ *const keys = [
+  'Code',
+  'Account type',
+  'Name',
+  'Street name',
+  'House number',
+  'Postal code',
+  'City',
+  'Email address',
+  'Phone number',
+  'Mobile number',
+  'Actions',
+];
+ */
 export default function Header(headerParent, keys, current, setCurrent) {
   const container = createNode(headerParent, {
     tag: 'div',
@@ -8,13 +25,29 @@ export default function Header(headerParent, keys, current, setCurrent) {
   });
 
   for (const key of keys) {
-    createNode(container, {
+    const keyHolder = createNode(container, {
+      tag: 'div',
+      classes: [styles.key],
+    });
+    createNode(keyHolder, {
       tag: 'div',
       classes: [styles.item],
       text: key,
       dataset: current.key === key ? current : null,
     }).addEventListener('click', function () {
-      setCurrent((prev) => (prev.key === key ? {...prev, sort: (prev.sort + 1) % 3} : {key, sort: 1}));
+      setCurrent((prev) => (prev.key === key ? {...prev, sort: (prev.sort + 1) % 3} : {...prev, key, sort: 1, dropdownOpen: false}));
+    });
+    const imageHolder = createNode(keyHolder, {
+      tag: 'div',
+      classes: [styles.dropdown],
+      dataset: current.key === key ? current : null,
+    });
+    createNode(imageHolder, {
+      tag: 'img',
+      src: './assets/images/ellipsis-vertical.svg',
+      dataset: current.key === key ? current : null,
+    }).addEventListener('click', function () {
+      setCurrent((prev) => ({...prev, key, dropdownOpen: true}));
     });
   }
 }
