f687810459833beacf1e4ff64259257833fb9470
Zlatan Bekric <zlaja@Zlatans-MacBook-Pro.local>
2024-11-14 18:32:20 +0100

    fix: exit image

--- script/components/exit.js
+++ script/components/exit.js
@@ -1,8 +1,8 @@
 import createNode from '../utils/createNode';
-
+import exit from '../../assets/images/Exit.svg';
 export default function Exit(parent) {
   const container = createNode(parent, {tag: 'div'});
   container.classList.add('exit-holder');
   const filterHolder = createNode(container, {tag: 'div'});
-  return createNode(filterHolder, {tag: 'img', src: './assets/images/Exit.svg'});
+  return createNode(filterHolder, {tag: 'img', src: exit});
 }
